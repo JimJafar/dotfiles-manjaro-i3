b3dcffa6cf7160943dc049b4ff773b466bdfb0cd
Jim Sangwine <jim@sangwine.net>
2018-02-28 10:51:47 +0800

    Make polybar load properly on multiple monitors

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -6,8 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar jim &
-
+# Make the bar load properly on multiple monitors
+# https://github.com/jaagr/polybar/issues/763#issuecomment-331604987
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload jim &
+  done
+else
+  polybar --reload jim &
+fi
 
 echo "Bars launched..."
