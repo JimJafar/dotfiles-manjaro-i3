abd7a3764d5eb029d22dca9b78bd562c2d89d011
Jim Sangwine <jim@sangwine.net>
2018-03-06 10:00:27 +0800

    Add nitrogen config and move workspace 6 to eDPI

--- scripts/lock-i3.sh
+++ scripts/lock-i3.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
 #icon="$HOME/dotfiles-manjaro-i3/img/i3-lock-icon-deathnote.png"
-icon="$HOME/dotfiles-manjaro-i3/img/i3-lock-icon-homestuck-crew.png"
+#icon="$HOME/dotfiles-manjaro-i3/img/i3-lock-icon-homestuck-crew.png"
 
 tmpbg='/tmp/screen.png'
+
+revert() {
+  xset dpms 0 0 0
+}
 
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -u -i "$tmpbg"
+#convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+convert "$tmpbg" -gravity center -composite -matte "$tmpbg"
+
+# enable DPMS while the screen is locked
+trap revert HUP INT TERM
+xset +dpms dpms 10 20 30
+
+# lock the screen
+i3lock --nofork --image="$tmpbg"
+# switch off DPMS
+revert
