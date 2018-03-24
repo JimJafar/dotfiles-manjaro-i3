054eff069e616f62abb64aee81a1292da6251ffa
Jim Sangwine <jim@sangwine.net>
2018-03-24 11:26:17 +0800

    Fixed natural scrolling script to work with more mice

--- scripts/natural-scrolling.sh
+++ scripts/natural-scrolling.sh
@@ -5,5 +5,5 @@ TOUCHPADID=`xinput list|grep Touchpad|grep -m 1 id=|cut -f 2|cut -f 2 -d =`
 xinput set-prop "${TOUCHPADID}" `xinput list-props "${TOUCHPADID}"|grep -m 1 "Natural Scrolling Enabled"|grep -oP '\(\K[^\)]+'` 1
 
 # Set natural scrolling on mouse
-MOUSEID=`xinput list|grep mouse|grep -m 1 id=|cut -f 2|cut -f 2 -d =`
+MOUSEID=`xinput list|grep -i mouse|grep -m 1 id=|cut -f 2|cut -f 2 -d =`
 xinput set-prop "${MOUSEID}" `xinput list-props "${MOUSEID}"|grep -m 1 "Natural Scrolling Enabled"|grep -oP '\(\K[^\)]+'` 1
