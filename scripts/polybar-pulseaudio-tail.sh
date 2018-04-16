2b83e2aa6d94f3dac34b33a2b9b85c596c790033
Jim Sangwine <jim@sangwine.net>
2018-04-16 23:44:05 +0800

    Back to i3 :) - re-themed wpgtk to use the current GTK theme instead of wallpaper colours

---
+++ scripts/polybar-pulseaudio-tail.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+sink=0
+
+volume_up() {
+    pactl set-sink-volume $sink +1%
+}
+
+volume_down() {
+    pactl set-sink-volume $sink -1%
+}
+
+volume_mute() {
+    pactl set-sink-mute $sink toggle
+}
+
+volume_print() {
+    if [ "$(pacmd list-sinks | grep active | head -n 1 | grep speaker)" ]; then
+        icon="#1"
+    elif [ "$(pacmd list-sinks | grep active | head -n 1 | grep headphones)" ]; then
+        icon="#2"
+    else
+        icon="#3"
+    fi
+
+    muted=$(pamixer --sink $sink --get-mute)
+
+    if [ "$muted" = true ]; then
+        echo "$icon --"
+    else
+        echo "$icon $(pamixer --sink $sink --get-volume)"
+    fi
+}
+
+listen() {
+    volume_print
+
+    pactl subscribe | while read -r event; do
+        if echo "$event" | grep -q "#$sink"; then
+            volume_print
+        fi
+    done
+}
+
+case "$1" in
+    --up)
+        volume_up
+        ;;
+    --down)
+        volume_down
+        ;;
+    --mute)
+        volume_mute
+        ;;
+    *)
+        listen
+        ;;
+esac
